0c1268e4c9e20d1ba42efd6c6b0a0acd29dbe6df
Kumari Shrividya Mol <90685998+kumari-shrividya@users.noreply.github.com>
2024-11-11 11:42:50 +0530

    Update find-peek-elmts.js

--- find-peek-elmts.js
+++ find-peek-elmts.js
@@ -1,19 +1,30 @@
 //An element is considered to be peak if its value is greater 
 //than or equal to the values of its adjacent elements (if they exist).
 
-function findPeekElmts(arr) {
+function findPeakElement(arr) {
+    const n = arr.length;
 
-    let peek = [];
+    for (let i = 0; i < n; i++) {
+        const left = i > 0 ? arr[i - 1] : -Infinity; // Adjacent left value
+        const right = i < n - 1 ? arr[i + 1] : -Infinity; // Adjacent right value
 
-    for (let i = 0; i < arr.length - 1; i++) {
-
-        if(arr[i] > arr[i+1]) {
-
-            peek.push(i)
+        if (arr[i] >= left && arr[i] >= right) {
+            return i; // Return the index of the peak
         }
     }
-    return peek;
+
+    return -1; // Return -1 if no peak (edge case)
 }
 
-const arr =[1, 1, 1, 2, 1, 1, 1];
-console.log(findPeekElmts(arr))
+// Validator to check the correctness of the returned peak index
+function validatePeakIndex(arr, peakIndex) {
+    const left = peakIndex > 0 ? arr[peakIndex - 1] : -Infinity;
+    const right = peakIndex < arr.length - 1 ? arr[peakIndex + 1] : -Infinity;
+
+    return arr[peakIndex] >= left && arr[peakIndex] >= right;
+}
+
+const arr = [1, 2, 3, 1];
+const peakIndex = findPeakElement(arr);
+console.log(peakIndex); // Output: 2
+console.log(validatePeakIndex(arr, peakIndex)); // Output: true
